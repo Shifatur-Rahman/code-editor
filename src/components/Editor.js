ebe815243676adb7bcc8d2f3135ec0a670138b9a
Shifatur-Rahman <rjnovel28@gmail.com>
2023-10-29 15:22:22 +0600

    status add

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -6,26 +6,34 @@ import "codemirror/mode/xml/xml";
 import "codemirror/mode/css/css";
 import "./Style.css"
 // import {UnControlled as CodeMirror} from 'react-codemirror2'
+import {Controlled as CodeMirror} from 'react-codemirror2'
 
 const Editor = (props) => {
-
     const {language,displayName,value,onChange} = props;
     function handleChange(editor, data, value){
        onChange(value);
     }
 
+    var editors = document.getElementsByClassName('code-mirror-wrapper');
+    for (let i = 1; i < editors.length; i++) {
+        editors[i].remove()
+    }
+
   return (
     <>
 
-          <div className="editor-container">
+          <div className="editor-container"> 
+
               <div className="editor-title">
                   {displayName}
                   <button>O/C</button>
               </div>
+
               <ControlledEditor
                   onBeforeChange={handleChange}
                   value={value}
-                  className="code-mirror-wrapper"
+                  // className="code-mirror-wrapper"
+                  className="CodeMirror"
                   options={{
                       lineWrapping: true,
                       lint: true,
@@ -34,9 +42,11 @@ const Editor = (props) => {
                       lineNumbers: true,
                   }}
               />
+              <ControlledEditor />
           </div>
     </>
   )
 }
 
 export default Editor
+
